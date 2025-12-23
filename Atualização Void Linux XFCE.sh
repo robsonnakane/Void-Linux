86396f69ff399d3b94b4e42ed7eb6494198f8c2b
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2025-12-23 10:49:49 -0300

    Add files via upload

--- Atualização Void Linux XFCE.sh
+++ Atualização Void Linux XFCE.sh
@@ -6,11 +6,11 @@
 ##Comandos no Super User##
 #sudo xbps-install -u xbps;
 sudo xbps-install -Su;
-#sudo xbps-install -S -y xtools;
+#sudo xbps-install -S -y xtools flatpak;
 #flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo;
 xcheckrestart;
 
-#sudo xbps-install -S -y fastfetch simple-scan thunderbird audacious gimp transmission-gtk rpi-imager firefox gwenview kate kdenlive yt-dlp xfburn audacity inkscape lutris;
+#sudo xbps-install -S -y fastfetch simple-scan thunderbird audacious gimp transmission-gtk rpi-imager firefox gwenview kate kdenlive yt-dlp xfburn audacity inkscape lutris kitty;
 
         ##Instalação dos pacotes flatpaks##
 #sudo flatpak install flathub com.spotify.Client -y; sudo flatpak install flathub com.valvesoftware.Steam -y; sudo flatpak install flathub us.zoom.Zoom -y; sudo flatpak install flathub org.onlyoffice.desktopeditors -y; sudo flatpak install flathub com.adobe.Flash-Player-Projector -y; sudo flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; sudo flatpak install flathub org.chromium.Chromium -y; sudo flatpak install flathub org.fedoraproject.MediaWriter -y; sudo flatpak install flathub org.kde.kget -y; sudo flatpak install flathub org.videolan.VLC -y; sudo flatpak install flathub net.mkiol.SpeechNote -y; sudo flatpak install flathub com.saivert.pwvucontrol -y; sudo flatpak install flathub io.github.dvlv.boxbuddyrs -y;
