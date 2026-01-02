decd3217caf8c904796df3740a45927a3cf3f8f0
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2026-01-02 14:13:42 -0300

    Add files via upload

--- Atualização Void Linux XFCE.sh
+++ Atualização Void Linux XFCE.sh
@@ -3,17 +3,11 @@
       ###Void Linux by distrobox###
       ###Atualização completa do sistema###
 
-##baixar o shell script do github
-#doas xbps-install -S -y git;
-#git clone https://github.com/robsonnakane/Void-Linux.git;
-#chmod +x 'nome do arquivo';
-
 ##Configuração do 'doas' como su (copiar e colar no terminal antes da instalação)
-xbps-install -u -y xbps; xbps-install -Su -y; xbps-install -y opendoas; usermod -aG wheel robsonnakane; echo "permit persist :wheel" > /etc/doas.conf; chown root:root /etc/doas.conf; chmod 400 /etc/doas.conf
+#xbps-install -u -y xbps; xbps-install -Su -y; xbps-install -y opendoas; usermod -aG wheel robsonnakane; echo "permit persist :wheel" > /etc/doas.conf; chown root:root /etc/doas.conf; chmod 400 /etc/doas.conf
 
 ##Atualização do Void Linux##
-#doas xbps-install -u xbps;
-doas xbps-install -Su;
+doas xbps-install -Su -y;
 #doas xbps-install -S -y flatpak xtools rsync nano;
 #flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo;
 xcheckrestart;
