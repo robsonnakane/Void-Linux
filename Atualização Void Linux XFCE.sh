76964876543a29b8d1fa9762fed4cd3f8348d547
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2026-01-02 12:02:02 -0300

    Add files via upload

--- Atualização Void Linux XFCE.sh
+++ Atualização Void Linux XFCE.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
-            ###Void Linux by distrobox###
-            ###Atualização completa do sistema###
+      ###Void Linux by distrobox###
+      ###Atualização completa do sistema###
 
 ##baixar o shell script do github
 #doas xbps-install -S -y git;
 #git clone https://github.com/robsonnakane/Void-Linux.git;
 #chmod +x 'nome do arquivo';
 
-##Comandos no Super User##
+##Configuração do 'doas' como su (copiar e colar no terminal antes da instalação)
+#xbps-install -Su; xbps-install -y opendoas; usermod -aG wheel robsonnakane; echo "permit persist :wheel" > /etc/doas.conf; chown root:root /etc/doas.conf; chmod 400 /etc/doas.conf; exit
+
+##Atualização do Void Linux##
 #doas xbps-install -u xbps;
-sudo xbps-install -Su;
-#doas xbps-install -S -y void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree;
-#doas xbps-install -S -y flatpak xtools rsync;
+doas xbps-install -Su;
+#doas xbps-install -S -y flatpak xtools rsync nano;
 #flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo;
 xcheckrestart;
 
-        ##Instalação de pacotes##
+    ##Instalação de pacotes##
 #doas xbps-install -S -y fastfetch simple-scan thunderbird audacious gimp transmission-gtk rpi-imager firefox gwenview kate kdenlive yt-dlp xfburn audacity inkscape lutris kitty;
 
-        ##Instalação dos pacotes flatpaks##
-#sudo flatpak install flathub com.spotify.Client -y; sudo flatpak install flathub com.valvesoftware.Steam -y; sudo flatpak install flathub us.zoom.Zoom -y; sudo flatpak install flathub org.onlyoffice.desktopeditors -y; sudo flatpak install flathub com.adobe.Flash-Player-Projector -y; sudo flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; sudo flatpak install flathub org.chromium.Chromium -y; sudo flatpak install flathub org.fedoraproject.MediaWriter -y; sudo flatpak install flathub org.kde.kget -y; sudo flatpak install flathub org.videolan.VLC -y; sudo flatpak install flathub net.mkiol.SpeechNote -y; sudo flatpak install flathub com.saivert.pwvucontrol -y;
+    ##Instalação dos pacotes flatpaks##
+#flatpak install flathub com.spotify.Client -y; flatpak install flathub com.valvesoftware.Steam -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; flatpak install flathub org.chromium.Chromium -y; flatpak install flathub org.fedoraproject.MediaWriter -y; flatpak install flathub org.kde.kget -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub net.mkiol.SpeechNote -y; flatpak install flathub com.saivert.pwvucontrol -y;
 
-        ##Bluetooth##
-#rfkill unblock bluetooth; sudo xbps-install -S -y bluez bluez-alsa libspa-bluetooth; sudo sv restart bluetoothd; sudo sv restart dbus; sudo sv restart sshd;
+    ##Bluetooth##
+#rfkill unblock bluetooth; xbps-install -S -y bluez bluez-alsa libspa-bluetooth; sv restart bluetoothd; sv restart dbus; sv restart sshd;
 
 
-        ##Atualização do Flatpak##
+    ##Atualização do Flatpak##
 flatpak update -y;
 
-systemctl reboot
+doas reboot
 
-        ##Baixar um vídeo em melhor qualidade:##
+    ##Baixar um vídeo em melhor qualidade:##
 #distrobox enter archlinux -- yt-dlp URL
-        ##Baixar só áudio (MP3):
+    ##Baixar só áudio (MP3):
 #distrobox enter archlinux -- yt-dlp -x --audio-format mp3 URL
 
-        ##Realização de backup##
+    ##Realização de backup##
 #doas rsync -avzrp --delete /home/robsonnakane/'Robson Nakane'/ robsonnakane@192.168.15.15:/home/robsonnakane/lenovo/
-        ##Recuperação de backup
+    ##Recuperação de backup
 #doas rsync -avzrp --delete robsonnakane@192.168.15.15:/home/robsonnakane/lenovo/ /home/robsonnakane/'Robson Nakane'/
