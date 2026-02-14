da6af0d77a0190a18fa1f28ce217d85726403bc3
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2026-02-14 10:42:15 -0300

    Add files via upload

--- Atualização VoidLinuxBR KDE by distrobox.sh
+++ Atualização VoidLinuxBR KDE by distrobox.sh
@@ -4,9 +4,9 @@
       ###Atualização completa do sistema###
 
 ##Configuração do 'doas' como su (copiar e colar no terminal antes da instalação, passo a passo)
-#xbps-install -uy xbps
-#xbps-install -Suy
-#xbps-install -Sy opendoas
+#vinstall -uy xbps
+#vinstall -Suy
+#vinstall -Sy opendoas
 #usermod -aG wheel robsonnakane
 #echo "permit persist :wheel" > /etc/doas.conf
 #chown root:root /etc/doas.conf
@@ -14,14 +14,15 @@
 #log out
 
 ##Atualização do Void Linux##
-doas xbps-install -Suy; #rodar a primeira atualização sem a instalação dos pacotes#
+vinstall -Suy; #rodar a primeira atualização sem a instalação dos pacotes#
+vinstall -Sycc;
 xcheckrestart;
 
     ##Instalação de pacotes KDE oficial##
-#doas xbps-install -Sy kitty podman docker crun rsync flatpak xtools rsync nano;
+#vinstall -Sy kitty podman docker crun rsync flatpak xtools nano;
 
     ##Pacotes Voidbr / Chililinux##
-#doas xbps-install -Sy voidbr-distrobox; doas xbps-install -Sy voidbr-lynxfetch; doas xbps-install -Sy chili-iso2usb; doas xbps-install -Sy chili-tradutor-go; doas xbps-install -Sy voidbr-vinstall;
+#vinstall -Sy voidbr-distrobox voidbr-lynxfetch chili-iso2usb chili-tradutor-go;
 
 #flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo;
 
@@ -32,19 +33,19 @@ xcheckrestart;
         ##Archlinux##
 #distrobox create -Y --name archlinux --image docker.io/library/archlinux:latest;
         ##Fedora##
-#distrobox create -Y --name fedora --image quay.io/fedora/fedora-toolbox:rawhide;
+#distrobox create -Y --name fedora --image quay.io/fedora/fedora:rawhide;
 
 distrobox-upgrade --all -v;
 
         ##Instalação dos pacotes nos containeres distrobox##
-#distrobox enter archlinux -- sudo pacman -S --noconfirm fastfetch simple-scan thunderbird audacious gimp transmission-gtk rpi-imager firefox gwenview kate kdenlive yt-dlp xfburn;
-#distrobox enter fedora -- sudo dnf5 install -y audacity inkscape boxes;
+#distrobox enter archlinux -- sudo pacman -S --noconfirm fastfetch simple-scan thunderbird audacious gimp transmission-gtk rpi-imager firefox gwenview kate kdenlive yt-dlp xfburn kcalc spectacle;
+#distrobox enter fedora -- sudo dnf5 install -y audacity inkscape;
 
         ##Exportação dos pacotes instalados no distrobox archlinux##
-#distrobox enter archlinux -- distrobox-export --app simple-scan; distrobox enter archlinux -- distrobox-export --app thunderbird; distrobox enter archlinux -- distrobox-export --app audacious; distrobox enter archlinux -- distrobox-export --app gimp; distrobox enter archlinux -- distrobox-export --app transmission-gtk; distrobox enter archlinux -- distrobox-export --app rpi-imager; distrobox enter archlinux -- distrobox-export --app firefox; distrobox enter archlinux -- distrobox-export --app gwenview; distrobox enter archlinux -- distrobox-export --app kate; distrobox enter archlinux -- distrobox-export --app kdenlive distrobox enter archlinux -- distrobox-export --app xfburn;
+#distrobox enter archlinux -- distrobox-export --app simple-scan; distrobox enter archlinux -- distrobox-export --app thunderbird; distrobox enter archlinux -- distrobox-export --app audacious; distrobox enter archlinux -- distrobox-export --app gimp; distrobox enter archlinux -- distrobox-export --app transmission-gtk; distrobox enter archlinux -- distrobox-export --app rpi-imager; distrobox enter archlinux -- distrobox-export --app firefox; distrobox enter archlinux -- distrobox-export --app gwenview; distrobox enter archlinux -- distrobox-export --app kate; distrobox enter archlinux -- distrobox-export --app kdenlive; distrobox enter archlinux -- distrobox-export --app xfburn; distrobox enter archlinux -- distrobox-export --app kcalc; distrobox enter archlinux -- distrobox-export --app spectacle;
 
         ##Exportação dos pacotes instalados no distrobox fedora##
-#distrobox enter fedora -- distrobox-export --app audacity; distrobox enter fedora -- distrobox-export --app inkscape boxes audacity;
+#distrobox enter fedora -- distrobox-export --app audacity; distrobox enter fedora -- distrobox-export --app inkscape;
 
         ##Instalação do lutris dentro do terminal distrobox debian##
 #echo -e "Types: deb\nURIs: https://download.opensuse.org/repositories/home:/strycore/Debian_12/\nSuites: ./\nComponents: \nSigned-By: /etc/apt/keyrings/lutris.gpg" | sudo tee /etc/apt/sources.list.d/lutris.sources > /dev/null
@@ -54,10 +55,10 @@ distrobox-upgrade --all -v;
 #distrobox-export --app lutris
 
         ##Instalação dos pacotes flatpaks##
-#flatpak install flathub com.spotify.Client -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; flatpak install flathub org.chromium.Chromium -y; flatpak install flathub org.fedoraproject.MediaWriter -y; flatpak install flathub org.kde.kget -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub net.mkiol.SpeechNote -y; flatpak install flathub com.saivert.pwvucontrol -y; flatpak install flathub io.github.dvlv.boxbuddyrs -y; flatpak install flathub org.telegram.desktop -y; flatpak install flathub com.obsproject.Studio -y;
+#flatpak install flathub com.spotify.Client -y;flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; flatpak install flathub org.chromium.Chromium -y; flatpak install flathub org.fedoraproject.MediaWriter -y; flatpak install flathub org.kde.kget -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub net.mkiol.SpeechNote -y; flatpak install flathub com.saivert.pwvucontrol -y; flatpak install flathub io.github.dvlv.boxbuddyrs -y; flatpak install flathub org.telegram.desktop -y; flatpak install flathub com.obsproject.Studio -y;
 
     ##Atualização do Flatpak##
-doas flatpak update -y;
+flatpak update -y;
 
 doas reboot
 
